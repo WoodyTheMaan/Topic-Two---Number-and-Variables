a862243fd6f2249539c871aea4221d18f0f5324f
Josiah Brownlee <buzz72woody@gmail.com>
2023-09-14 10:58:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Topic_Two___Number_and_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, passwordUseless;
+            int yearOfBirth;
+            Console.WriteLine("Hello, to continue please enter your name.");
+            name = Console.ReadLine();
+            Console.WriteLine($"Thanks {name} please wait while we get your account setup this may take a moment....");
+            Thread.Sleep(7000);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Thanks for waiting {name} please create a password below.");
+            passwordUseless = Console.ReadLine();
+            Thread.Sleep(1000); 
+            Console.WriteLine($"Thanks, now, please type how old you are");
+            yearOfBirth = Convert.ToInt32(Console.ReadLine());
+            Thread.Sleep(1000);
+            Console.WriteLine($"Thanks {name} ");
+            string year = DateTime.Parse(DateTime.Now.ToString()).Year.ToString();
+            Convert.ToInt32(year);
+            Console.WriteLine(yearOfBirth + year);
+
+
+        }
+    }
+}
